20a3515fb8e094ce1c1dc54d1b1ce37a6ca9407e
Murray Dunne <mdunne@compute3.esg.uwaterloo.ca>
2022-12-14 22:44:44 -0500

    Question generation with grammar corrections.

--- load-ontology.py
+++ load-ontology.py
@@ -1,5 +1,68 @@
 from owlready2 import *
+import glob
+import os
+import sys
 
-onto_legal =  get_ontology("lkif-core-master/legal-action.owl").load()
+i = 0
+for path in glob.glob(sys.argv[1]):
+    print(path)
+    get_ontology(path).load().save(file='test' + str(i) + '.ntriples', format='ntriples')
+    i += 1
 
-print(list(onto_legal.classes()))
+
+base = 0
+result = ''
+for path in glob.glob('test*'):
+    with open(path) as f:
+        print(path)
+        contents = f.read()
+        if '_:' in contents:
+            max_count = int(contents[contents.rindex('_:') + 2: contents.index(' ', contents.rindex('_:')+2)])
+            
+            for id in range(1, max_count+1):
+                contents = contents.replace('_:' + str(id), '_:' + str(id + base))
+
+            base += max_count
+        result = result + contents
+
+with open('condensed.ntriples', 'w') as f:
+    f.write(result)
+
+for path in glob.glob("test*.ntriples"):
+    os.remove(path)
+
+        
+    
+
+# onto_path.append("/home/mdunne/cs848fall2022/lkif-core-master/")
+# legal_onto = get_ontology('lkif-core-master/legal-action.owl').load()
+
+# legal_onto.save(file='test', format='ntriples')
+
+
+# # load_queue = [onto_legal]
+
+# # while len(load_queue) > 0:
+# #     onto = load_queue.pop()
+
+# #     for x in onto.imported_ontologies:
+# #         load_queue.append(x)
+
+
+# for cls in legal_onto.classes():
+#     #print(cls.name)
+#     if cls.name == 'Mandate':
+#         print(cls.name)
+#         for restrict in cls.is_a:
+#             print(restrict)
+
+#     # for y in onto.object_properties():
+#     #     print("PROP:", y)
+#     #     for z in y.get_relations():
+#     #         print("\t", z)
+
+#     # for y in onto.classes():
+#     #     print("CLASS:", y)
+#     #     for z in y.is_a:
+#     #         print("\t", z)
+
--- question-asking.py
+++ question-asking.py
@@ -1,10 +1,13 @@
+from gramformer import Gramformer
+import sys
+
 #########################################################
 ###### PHASE 1: LOAD THE ONTOLOGY FROM NTRIPLES #########
 #########################################################
 
 # first load the ontology text lines
 ontology_triples = []
-with open('condensed.ntriples') as f:
+with open(sys.argv[1]) as f:
     for line in f.readlines():
         split = line.strip().split(' ')
         s = split[0]
@@ -31,8 +34,18 @@ for s, p, o in ontology_triples:
     if p.endswith('#type') and o.endswith('owl#Class') and '_:' not in s:
         classes.add(s)
 
+# and also their labels
+labels = dict()
+for s, p, o in ontology_triples:
+    if p.endswith('#label'):
+        labels[s] = o[1:-1]
+
+print('classes', classes)
+print('labels', labels)
+
 # and assign them subclasses
 subclass_map = dict()
+superclass_map = dict()
 for s, p, o in ontology_triples:
     if p.endswith('#subClassOf'):
         # this is a subclass, but it could be a restriction subclass
@@ -44,6 +57,13 @@ for s, p, o in ontology_triples:
         if o not in subclass_map:
             subclass_map[o] = set()
         subclass_map[o].add(s)
+
+        if s not in superclass_map:
+            superclass_map[s] = set()
+        superclass_map[s].add(o)
+
+print('subclasses', subclass_map)
+print('superclasses', superclass_map)
 
 # now we can load all the object properties
 object_properties = set()
@@ -58,7 +78,11 @@ restrictions = []
 for s, p, o in ontology_triples:
     if p.endswith('owl#onProperty'): 
         
-        # o is the property, what is the target type
+        # o is the property, sometimes it's not defined
+        if o not in object_properties:
+            object_properties.add(o)
+        
+        # what is the target type
         target_type = None
         # this search is n^2 and could be optimized with a map, but the
         # ontologies are small enough it's irrelevant
@@ -98,7 +122,66 @@ for s, p, o in ontology_triples:
 
         # add the restriction
         restrictions.append((target_subject, o, target_type))
-                
+
+print('properties', object_properties)
+print(restrictions)        
+
+# now we restructure the restrictions taking into account the class
+# heirarchy so all relations are in the subclasses
+
+# this is a list of sets of prediates by subject
+domains = dict()
+# and this is a list of sets of objects by predicate
+ranges = dict()
+
+for s, p, o in restrictions:
+    if s not in domains.keys():
+        domains[s] = set()
+    if p not in ranges.keys():
+        ranges[p] = set()
+    
+    domains[s].add(p)
+    ranges[p].add(o)
+
+    if s in subclass_map.keys():
+        considered_subclasses = set()
+        unconsidered_subclasses = set()
+
+        unconsidered_subclasses.intersection_update(subclass_map[s])
+        while len(unconsidered_subclasses) > 0:
+            cls = unconsidered_subclasses.pop()
+
+            if cls not in domains.keys():
+                domains[cls] = set()
+            domains[cls].add(p)
+            
+            considered_subclasses.add(cls)
+            if cls in subclass_map.keys():
+                new_sub = subclass_map[cls].copy()
+                new_sub.difference_update(considered_subclasses)
+                unconsidered_subclasses = unconsidered_subclasses.union(new_sub)
+
+    if o in subclass_map.keys():
+        considered_subclasses = set()
+        unconsidered_subclasses = set()
+
+        unconsidered_subclasses.intersection_update(subclass_map[o])
+        while len(unconsidered_subclasses) > 0:
+            cls = unconsidered_subclasses.pop()
+
+            ranges[p].add(cls)
+            
+            considered_subclasses.add(cls)
+
+            if cls in subclass_map.keys():
+                new_sub = subclass_map[cls].copy()
+                new_sub.difference_update(considered_subclasses)
+                unconsidered_subclasses = unconsidered_subclasses.union(new_sub)
+
+print(domains)
+print('ranges')
+print(ranges)
+
 
 # for s, p, o in restrictions:
 #     print(s, p, o)
@@ -113,11 +196,30 @@ for s, p, o in ontology_triples:
 ###### PHASE 2: GENERATE FIRST ORDER QUESTIONS ##########
 #########################################################
 
+import spacy
+nlp = spacy.load("en_core_web_trf")
+
+gf = Gramformer(models = 1, use_gpu=True)
+
 def clear(x):
-    return x.strip().split('#')[-1].replace('_', ' ')
+    if x in labels.keys():
+        return labels[x]
+    else:
+        return x.strip().split('#')[-1].split('/')[-1].replace('_', ' ')
 
-document_object = 'legal-action.owl#Decision'
+document_subject = sys.argv[2]
 
-for s, p, o in restrictions:
-    if s == document_object:
-        print('What', clear(o), 'is this', clear(document_object), clear(p) + '?')
+for p in domains[document_subject]:
+    for o in ranges[p]:
+        question = 'What ' + clear(document_subject) + ' ' + clear(p) + ' this ' + clear(o) + '?'
+        print(question)
+        good_question = gf.correct(question, max_candidates=1).pop()
+        print(good_question)
+
+
+# for s, p, o in restrictions:
+#     if s == document_object:
+#         bad_question = 'What ' + clear(o) + ' is this ' + clear(document_object) + ' ' + clear(p) + '?' 
+#         good_question = gf.correct(bad_question, max_candidates=1)
+#         print(bad_question)
+#         print(good_question)
