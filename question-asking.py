6d9dd73e0ed9e98f831aa207abd2d4a4e05779d8
Murray Dunne <mdunne@compute3.esg.uwaterloo.ca>
2022-12-15 18:38:51 -0500

    Restrictions, data properties, and object properties fixes.

--- question-asking.py
+++ question-asking.py
@@ -1,6 +1,8 @@
 from gramformer import Gramformer
 import sys
 
+print_all = False
+
 #########################################################
 ###### PHASE 1: LOAD THE ONTOLOGY FROM NTRIPLES #########
 #########################################################
@@ -40,8 +42,9 @@ for s, p, o in ontology_triples:
     if p.endswith('#label'):
         labels[s] = o[1:-1]
 
-print('classes', classes)
-print('labels', labels)
+if print_all:
+    print('classes', classes)
+    print('labels', labels)
 
 # and assign them subclasses
 subclass_map = dict()
@@ -62,8 +65,9 @@ for s, p, o in ontology_triples:
             superclass_map[s] = set()
         superclass_map[s].add(o)
 
-print('subclasses', subclass_map)
-print('superclasses', superclass_map)
+if print_all:
+    print('subclasses', subclass_map)
+    print('superclasses', superclass_map)
 
 # now we can load all the object properties
 object_properties = set()
@@ -123,8 +127,40 @@ for s, p, o in ontology_triples:
         # add the restriction
         restrictions.append((target_subject, o, target_type))
 
-print('properties', object_properties)
-print(restrictions)        
+# if there are object properties defined with both a domain and a range, then we also include them
+# as restrictions, since there's very little difference
+for object_prop in object_properties:
+    domain_type = None
+    range_type = None
+    for s, p, o in ontology_triples:
+        if s.endswith(object_prop) and p.endswith('rdf-schema#domain'):
+            domain_type = o
+        if s.endswith(object_prop) and p.endswith('rdf-schema#range'):
+            range_type = o
+
+    if domain_type != None and range_type != None and domain_type in classes:
+        if range_type in classes:
+            restrictions.append((domain_type, object_prop, range_type))
+        else:
+            # there is a compound range, we have to traverse the tree to find all the components
+            # technically n^x again, but ontology is small enough for it to be irrelevant
+            to_check = [range_type]
+            find_limit = 0
+            while len(to_check) > 0 and find_limit < 20:
+                checking = to_check.pop()
+                for s2, p2, o2 in ontology_triples:
+                    if s2 == checking:
+                        if o2 in classes:
+                            restrictions.append((domain_type, object_prop, o2))
+                        else:
+                            to_check.append(o2)
+                find_limit += 1
+            
+            # do nothing if we hit the find limit
+
+if print_all:
+    print('properties', object_properties)
+    print(restrictions)        
 
 # now we restructure the restrictions taking into account the class
 # heirarchy so all relations are in the subclasses
@@ -178,10 +214,30 @@ for s, p, o in restrictions:
                 new_sub.difference_update(considered_subclasses)
                 unconsidered_subclasses = unconsidered_subclasses.union(new_sub)
 
-print(domains)
-print('ranges')
-print(ranges)
+if print_all:
+    print(domains)
+    print('ranges')
+    print(ranges)
 
+# now we load the data properties
+data_properties = []
+for s, p, o in ontology_triples:
+    if p.endswith('#type') and o.endswith('owl#DatatypeProperty'): 
+        domain_type = None
+        range_type = None
+        # n^2 again, but also irrelevant for small n
+        for s2, p2, o2 in ontology_triples:
+            if s2 == s and p2.endswith('rdf-schema#domain'):
+                domain_type = o2
+            if s2 == s and p2.endswith('rdf-schema#range'):
+                range_type = o2
+        
+        if domain_type != None and range_type != None and domain_type in classes:
+            data_properties.append((domain_type, s, range_type))
+
+if print_all:
+    print('data props')
+    print(data_properties)
 
 # for s, p, o in restrictions:
 #     print(s, p, o)
@@ -196,7 +252,15 @@ print(ranges)
 ###### PHASE 2: GENERATE FIRST ORDER QUESTIONS ##########
 #########################################################
 
+for s, p, o in data_properties:
+    print('data', s, p, o)
+
+for s, p, o in restrictions:
+    print('object', s, p, o)
+exit()
+
 import spacy
+import re
 nlp = spacy.load("en_core_web_trf")
 
 gf = Gramformer(models = 1, use_gpu=True)
@@ -205,7 +269,11 @@ def clear(x):
     if x in labels.keys():
         return labels[x]
     else:
-        return x.strip().split('#')[-1].split('/')[-1].replace('_', ' ')
+        spacified = x.strip().split('#')[-1].split('/')[-1].replace('_', ' ')
+        # convert camel case to space case from 
+        # https://stackoverflow.com/questions/5020906/python-convert-camel-case-to-space-delimited-using-regex-and-taking-acronyms-in
+        return re.sub(r'((?<=[a-z])[A-Z]|(?<!\A)[A-Z](?=[a-z]))', r' \1', spacified)
+
 
 document_subject = sys.argv[2]
 
